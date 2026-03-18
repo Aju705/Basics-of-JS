c01ce11dc0c1651bb1c3378c1d22f74a85ba8112
Aju705 <ajinkyanagawade2000@gmail.com>
2026-03-19 01:55:31 +0530

    random colot generation on start

---
+++ Small_project/Start stop random color/main.js
@@ -0,0 +1,37 @@
+//generate the random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random()* 16)]
+    }
+    return color
+}
+let intervalId;
+const startChangingColor = function(){
+    if(!intervalId){
+        console.log("start is pressed and null check is pass");
+        
+        intervalId = setInterval(changebgColor, 1000)
+    }
+    
+    function changebgColor () {
+        document.body.style.backgroundColor = randomColor()
+    }
+    
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null
+    console.log("stop press and null is pass");
+    
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
+
+
+    
